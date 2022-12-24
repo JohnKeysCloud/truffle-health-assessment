8bfc8186bf82e346efd247ffc7b78151d7e9e838
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2022-12-24 02:55:04 -0500

    refactor more info modal, add styles

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 
 const closeModalButton = document.querySelector('.modal-close-button');
+const closeModalButtonCSS = document.querySelector('.modal-close-button-css');
 const uploadModal = document.querySelector('#upload-modal');
 const openModalButton = document.querySelector('.modal-open-button');
 const formButton = document.querySelector('.file-form .button');
+const billTable = document.querySelector('.full-help-modal');
 openModalButton.addEventListener('click', () => uploadModal.showModal());
 closeModalButton.addEventListener('click', () => uploadModal.close(uploadModal));
 
-const
+const moreInfoButton = document.querySelectorAll('.more-info-button');
+
+for (let i = 0; i < moreInfoButton.length; ++i) {
+    moreInfoButton[i].addEventListener('click', () => {
+        billTable.classList.toggle('open');
+    })
+}
+
+closeModalButtonCSS.addEventListener('click', () =>  billTable.classList.toggle('open'))
