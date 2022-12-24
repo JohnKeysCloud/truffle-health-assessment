f7ee24651f197bf4c044c03227a61415bea0833a
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2022-12-24 18:35:37 -0500

    clean js, re-link cash icon

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 
+const billTable = document.querySelector('.full-help-modal');
 const closeModalButton = document.querySelector('.modal-close-button');
 const closeModalButtonCSS = document.querySelector('.modal-close-button-css');
-const uploadModal = document.querySelector('#upload-modal');
-const openModalButton = document.querySelector('.modal-open-button');
 const formButton = document.querySelector('.file-form .button');
-const billTable = document.querySelector('.full-help-modal');
+const moreInfoButton = document.querySelectorAll('.more-info-button');
+const openModalButton = document.querySelector('.modal-open-button');
+const uploadModal = document.querySelector('#upload-modal');
 openModalButton.addEventListener('click', () => uploadModal.showModal());
 closeModalButton.addEventListener('click', () => uploadModal.close(uploadModal));
-
-const moreInfoButton = document.querySelectorAll('.more-info-button');
 
 for (let i = 0; i < moreInfoButton.length; ++i) {
     moreInfoButton[i].addEventListener('click', () => {
