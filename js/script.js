31725fa430db73ea643c133e291d1a4e7f242082
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2022-12-25 06:08:44 -0500

    clean js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,15 @@
 const billTable = document.querySelector('.full-help-modal');
 const closeModalButton = document.querySelector('.modal-close-button');
 const closeModalButtonCSS = document.querySelector('.modal-close-button-css');
-const formButton = document.querySelector('.file-form .button');
 const moreInfoButton = document.querySelectorAll('.more-info-button');
 const openModalButton = document.querySelector('.modal-open-button');
 const uploadModal = document.querySelector('#upload-modal');
-openModalButton.addEventListener('click', () => uploadModal.showModal());
-closeModalButton.addEventListener('click', () => uploadModal.close(uploadModal));
 
-for (let i = 0; i < moreInfoButton.length; ++i) {
-    moreInfoButton[i].addEventListener('click', () => {
-        billTable.classList.toggle('open');
-    })
+function toggleBillModal() {
+    billTable.classList.toggle('open');
 }
 
-closeModalButtonCSS.addEventListener('click', () =>  billTable.classList.toggle('open'))
+[...moreInfoButton].forEach(button => button.addEventListener('click', toggleBillModal));
+closeModalButton.addEventListener('click', () => uploadModal.close(uploadModal));
+closeModalButtonCSS.addEventListener('click', () => billTable.classList.toggle('open'));
+openModalButton.addEventListener('click', () => uploadModal.showModal());
